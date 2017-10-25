1731b6f39c5fbb6d643678a85c2d5e8df33581f3
Enzo Gladiadis <enzotech2@icloud.com>
2017-10-25 16:15:44 +0000

    add-comments

--- main.cpp
+++ main.cpp
@@ -10,7 +10,8 @@ int main()
 {
     cout << "Are you sure u want to do this?" << endl;
 
-    system("shutdown -P now"); //For Linux.
+    system("shutdown -P now"); // For Linux.
+    system("c:\\windows\\system32\\shutdown /i"); // For Windows
 
     return 0;
 }
